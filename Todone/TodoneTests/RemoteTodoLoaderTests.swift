3b1fb46d3fe222327ddd7b9dba7e429c2fcd1e8f
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-01-11 16:56:31 +0100

    Add Domain Error .connectivity on client error and stub the response

--- Todone/TodoneTests/RemoteTodoLoaderTests.swift
+++ Todone/TodoneTests/RemoteTodoLoaderTests.swift
@@ -9,18 +9,26 @@ import XCTest
 
 class RemoteTodoLoader {
     private let client: HTTPClient
+    private let url: URL
     
-    init(client: HTTPClient) {
-        self.client = client
+    public enum Error: Swift.Error {
+        case connectivity
     }
     
-    func load(from url: URL) {
-        self.client.get(from: url)
+    init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load(completion: @escaping (RemoteTodoLoader.Error) -> Void = { _ in }) {
+        self.client.get(from: url) { error in
+            completion(.connectivity)
+        }
     }
 }
 
 protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error) -> Void)
 }
 
 final class RemoteTodoLoaderTests: XCTestCase {
@@ -32,20 +40,20 @@ final class RemoteTodoLoaderTests: XCTestCase {
     }
     
     func test_load_requestsDataFromURL() {
-        let (sut, client) = makeSUT()
         let url = URL(string: "https://example.com")!
+        let (sut, client) = makeSUT(url: url)
         
-        sut.load(from: url)
+        sut.load()
         
         XCTAssertNotNil(client.requestedURL)
     }
     
     func test_load_requestTwiceFromURL() {
-        let (sut, client) = makeSUT()
         let url = URL(string: "https://example.com")!
+        let (sut, client) = makeSUT(url: url)
         
-        sut.load(from: url)
-        sut.load(from: url)
+        sut.load()
+        sut.load()
         
         XCTAssertEqual(client.requestedURL, [url, url])
     }
@@ -54,23 +62,37 @@ final class RemoteTodoLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
         let url = URL(string: "https://example.com")!
         
-        sut.load(from: url)
+        sut.load()
         
         XCTAssertEqual(client.requestedURL.first, url)
     }
     
+    func test_load_delieversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+     
+        var capturedError: RemoteTodoLoader.Error?
+        sut.load { error in capturedError = error}
+        
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+    
     // MARK: Helpers
     
-    private func makeSUT() -> (RemoteTodoLoader, HTTPClientSpy) {
+    private func makeSUT(url: URL = URL(string: "https://example.com")!) -> (RemoteTodoLoader, HTTPClientSpy) {
         let client = HTTPClientSpy()
-        let sut = RemoteTodoLoader(client: client)
+        let sut = RemoteTodoLoader(client: client, url: url)
         return (sut, client)
     }
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL = [URL]()
+        var error: Error?
         
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURL.append(url)
         }
     }
