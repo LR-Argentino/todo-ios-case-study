4c55e0534820811563c59d4867584c184ebcf715
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-01-28 19:24:06 +0100

    Add busniess logic and test case

--- Todone/TodoneTests/Todo API Tests/TodoCreaterTests.swift
+++ Todone/TodoneTests/Todo API Tests/TodoCreaterTests.swift
@@ -15,11 +15,21 @@ protocol TodoRepositoryProtocol {
 class TodoCreater {
     let todoRepository: TodoRepositoryProtocol
     
+    enum TodoCreaterError: Error {
+        case emptyTitle
+    }
+    
     init(todoRepository: TodoRepositoryProtocol) {
         self.todoRepository = todoRepository
     }
     
-    func create(title: String, comment: String? = nil, priority: String, dueDate: Date, users: [UUID] = []) {
+    func create(title: String, comment: String? = nil, priority: String, dueDate: Date, users: [UUID] = []) throws {
+        let isTitleEmpty = title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+        
+        guard isTitleEmpty == false else {
+            throw TodoCreaterError.emptyTitle
+        }
+        
         let todo = TodoItem(title: title, comment: comment, priority: priority, dueDate: dueDate, users: users)
         
         todoRepository.save(todo: todo)
@@ -32,9 +42,20 @@ final class TodoCreaterTests: XCTestCase {
         let mockRepository = MockTodoRepository()
         let sut = TodoCreater(todoRepository: mockRepository)
         
-        sut.create(title: "Title", comment: "some comments", priority: "high", dueDate: Date().addingTimeInterval(10000), users: [])
+        try! sut.create(title: "Title", comment: "some comments", priority: "high", dueDate: Date().addingTimeInterval(10000), users: [])
         
         XCTAssertEqual(mockRepository.todos.count, 1)
+    }
+    
+    func test_create_shouldThrowErrorOnEmptyTitle() {
+        let mockRepository = MockTodoRepository()
+        let sut = TodoCreater(todoRepository: mockRepository)
+        
+        
+        XCTAssertThrowsError(try sut.create(title: "  ", comment: "some comments", priority: "high", dueDate: Date().addingTimeInterval(10000), users: []), "Title cannot be blank") { error in
+            XCTAssertEqual(error as? TodoCreater.TodoCreaterError, TodoCreater.TodoCreaterError.emptyTitle)
+            XCTAssertEqual(mockRepository.todos.count, 0)
+        }
     }
     
     // MARK: Helper
