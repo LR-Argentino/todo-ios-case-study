e21a44251e70f5a860bfaed74a398af23ef5df98
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-02-26 13:33:15 +0100

    Add Red Test

--- Todone/TodoneTests/CreateTodoTests.swift
+++ Todone/TodoneTests/CreateTodoTests.swift
@@ -6,9 +6,20 @@
 //
 
 import XCTest
+import Todone
 
 final class CreateTodoTests: XCTestCase {
-    func test_create_createsTodoSucces
-
-
+    func test_create_createsTodoSuccessfully() async throws {
+        // GIVEN
+        let userSesssion = AppleAuthenticationStub.signInWithAppleSuccessfully()
+        XCTAssertNotNil(userSesssion)
+        
+        // WHEN
+        let todo = TodoItem(title: "Check for vurnerabilities", priority: "high", dueDate: Date.now.addingTimeInterval(3600), users: [])
+        let todoService = TodoService()
+        try await todoService.create(todo: todo)
+        
+        // THEN
+        XCTAssertEqual(todos.count, 1)
+    }
 }
